e3ee03efbac2b47392a04195717d0de7d0dbb905
yangzhongcheng <332802744@qq.com>
2014-08-28 15:12:56 +0800

    nothing

--- ommp/deploy/setting.py
+++ ommp/deploy/setting.py
@@ -4,56 +4,18 @@ JENERAL_CONF = {'username' : 'ckt',
                 'port' : 22,
                 }
 
-WEBSITE_CONF = {'exclude' : ['.git', '.gitignore', 'config.py', 'fabfile.py', 'admin', 'hikeuc/admin.php',], 
+WEBSITE_CONF = {'exclude' : [], 
             'hosts' : [],
-            'resource' : '/app/webapps/hike',
-            'target' : '/app/webapps/hike', 
+            'resource' : '',
+            'target' : '', 
             'verification' : 'admin',
-            'script' : '/app/webapps/pre_deploy.sh',
-            'static' : {'./js/' : '/app/webapps/media/js', 
-                        './images/home/' : '/app/webapps/media/images/home', 
-                        './style/' : '/app/webapps/media/style', 
-                        './images/index/' : '/app/webapps/media/images/index',
-                        }, 
-            'static_host' : ['10.120.10.32',],
+            'script' : '',
             }
 
-WEIXIN_CONF = {'exclude' : ['.git', '.gitignore', 'config.py', 'fabfile.py', ], 
-               'hosts' : [],
-               'resource' : '/app/webapps/hike_weixin',
-               'target' : '/app/webapps/hike_weixin', 
-               'verification' : 'admin',
-               'script' : '/app/webapps/pre_weixin.sh',
-            }
-
-ENWEB_CONF = {'exclude' : ['.git', '.gitignore', 'config.py', 'fabfile.py', 'admin', 'hikeuc/admin.php',], 
-           'hosts' : [],
-           'resource' : '/app/webapps/hike_en',
-           'target' : '/app/webapps/hike_en', 
-           'verification' : 'admin',
-           'script' : '/app/webapps/pre_en.sh',
-            }
-
-CMS_CONF = {'exclude' : ['.git', '.gitignore', 'config.py', 'fabfile.py', 'admin', 'hikeuc/admin.php',], 
+BBS_CONF = {'exclude' : [], 
             'hosts' : [],
-            'resource' : '/app/webapps/hike',
-            'target' : '/app/webapps/hike', 
+            'resource' : '',
+            'target' : '', 
             'verification' : 'admin',
-            'script' : '/app/webapps/pre_cms.sh',
-            }
-
-MEDIA_CONF = {'exclude' : ['.git', '.gitignore', 'config.py', 'fabfile.py', 'admin', 'hikeuc/admin.php',], 
-              'hosts' : ['10.169.0.120',],
-              'resource' : '/app/webapps/hike_media',
-              'target' : '/app/webapps/hike_media', 
-              'verification' : 'admin',
-              'script' : '/app/webapps/pre_media.sh',
-            }
-
-BBS_CONF = {'exclude' : ['.git', '.gitignore', 'config.py', 'fabfile.py', 'data/attachment', 'data/attachment_local', 'data/diy', 'admin', 'admin.php'], 
-            'hosts' : ['10.120.10.32', '10.120.10.36', ],
-            'resource' : '/app/webapps/hike_bbs_x3',
-            'target' : '/app/webapps/hike_new', 
-            'verification' : 'admin',
-            'script' : '/app/webapps/pre_bbs.sh',
+            'script' : '',
             }
